c4bb80851947a5ffb801d93d2f23a448336ef586
mariaato <mariaaatonha@gmail.com>
2024-10-17 19:42:28 -0300

    Menu expansivo

--- pesquisar.php
+++ pesquisar.php
@@ -11,10 +11,13 @@ $pesquisar = $_POST['pesquisar'];
 $resultado = "SELECT * FROM anuncios WHERE proprietario LIKE '%$pesquisar%' LIMIT 5";
 $resultado_anuncio=mysqli_query($conexao, $resultado);
 
-while($rows_anuncio=mysqli_fetch_array($resultado_anuncio)){
+if ($rows_anuncio=mysqli_fetch_array($resultado_anuncio)){
     echo "Proprietario: ".$rows_anuncio['proprietario']."<br>";
     echo "Valor: ".$rows_anuncio['valor']."<br>";
     echo "Cidade: ".$rows_anuncio['cidade']."<br>";
     echo "<br>";
+}else{
+    echo "ERRO";
 }
+
 ?>
