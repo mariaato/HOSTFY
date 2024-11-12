df426017af3f91a32db8e445824bb3946c6b6380
mariaato <mariaaatonha@gmail.com>
2024-11-12 09:49:07 -0300

    estilo mensagem de bloqueio login.php

--- quemsomos.php
+++ quemsomos.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOSTFY</title>
+    <link rel="shortcut icon" href="logoHostfy.png">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="styles.css">
     <style>
