8efbc8cdb879733c31cba73e2e8ace3a3ee9b648
ReidoMahjong <igor.raimundo@aluno.fmpsc.edu.br>
2024-11-13 15:56:26 -0300

    pequenas alterações, ainda não está pronto

--- admin.php
+++ admin.php
@@ -4,23 +4,77 @@ session_start();
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="shortcut icon" href="logoHostfy.png">
+    <style> 
+        .body_error{ 
+            background-color:#FEF6EE;  
+            color: #C56126;
+            align-items: center; 
+            justify-content: center; 
+            }
+
+        .logo_error {
+            width: 120px; 
+            position: fixed; 
+            left: 50px; 
+            top: 35px;} 
+            
+        .a_error {
+            text-decoration: none; 
+            color: #5b2c12
+            }
+        
+        header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #FEF6EE;
+            padding: 15px;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        </style>
 </head>
 <body>
 
-    <!-- Caso seja o admin -->
+    <!-- caso seja o admin -->
     <div id='permitido'>
-        <p>admin logado</p>
+        <header>
+            <!-- Botão do ícone de menu -->
+            <button class="menu-icon" id="menu-toggle">
+                <i class='bx bx-menu'></i>
+            </button>
+
+            <img src="logoHostfy.png" alt="logo" class="logo" />
+
+            <!-- Campo de pesquisa -->
+            <form method="post" action="pesquisar.php" class="search-form">
+                <input type="text" name="pesquisar" class="search-input">
+                <span>
+                    <button type="submit" class="search-button">
+                        <i class='bx bx-search'></i>
+                    </button>
+                </span>
+            </form>
+            <a href="logout.php" class="menu__link">Sair</a>
+        </header>
+
+        
         <a href="logout.php">sair</a>
     </div>
     
-    <!-- Caso não seja o admin e acessem a pagina -->
+    <!-- caso não seja o admin e acessem a pagina -->
     <div id='negado'>
-        <p>Você não tem permissão para acessar essa página, volte ao <a href="index.php">início</a></p>
+    <br>
+        <div class="body_error" style=" background-color: #fff3f3; border: 1px solid orange; padding: 15px; border-radius: 8px; text-align: center; "> 
+            <img src="logoHostfy.png" alt="logo" class="logo_error">
+            <h1>Você não tem permissão para acessar essa página!</h1>
+            <p>Volte ao <a class="a_error" href="index.php">início.</a></p>
+        </div>   
     </div>
     
     <script>
