338de5a10d694b128decc549ec73f0d9ceaff050
mariaato <mariaaatonha@gmail.com>
2024-11-26 10:05:09 -0300

    imagem reserva array

--- reserva.php
+++ reserva.php
@@ -500,7 +500,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirmar_reserva']))
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
   </label>
 </div>
-    <img src="<?= htmlspecialchars($imovel['imagens']); ?>" alt="Imagem do Imóvel">
+<?php
+    //separa as imagens em um array
+    $imgs = explode(", ", $imovel['imagens']);
+?>
+    <img src="<?php echo $imgs[0]; ?>" >
+
     <div class="informação">
         <p><strong><?= htmlspecialchars($imovel['Cidade']); ?> - <?= htmlspecialchars($imovel['UF']); ?></strong></p>
         <span class="valor">R$ <?= number_format($imovel['Valor'], 2, ',', '.'); ?></span>
